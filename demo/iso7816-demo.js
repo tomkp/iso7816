5b2708111164ea676629d425977443d05b678bbd
tomkp <tom@tomkp.com>
2025-12-28 13:40:49 +0000

    Replace deprecated card-reader with smartcard package (#10)
    BREAKING CHANGE: The API signature has changed.
    
    Before:
      const iso7816 = require('iso7816');
      const application = iso7816(devices, reader);
    
    After:
      const iso7816 = require('iso7816');
      const application = iso7816(card);
    
    The smartcard package passes the card object directly which has a
    transmit() method, simplifying the API.
    
    Changes:
    - Updated dependency from card-reader to smartcard ^3.2.1
    - Simplified Iso7816 constructor to accept card object directly
    - Updated demo to use new smartcard Devices API
    - Updated README with new API documentation and examples
    
    Fixes #9

--- demo/iso7816-demo.js
+++ demo/iso7816-demo.js
@@ -1,39 +1,28 @@
-var devices = require('card-reader');
-var iso7816 = require('../lib/iso7816-application');
+const { Devices } = require('smartcard');
+const iso7816 = require('../lib/iso7816-application');
 
+const devices = new Devices();
 
-devices.on('device-activated', function (event) {
-    console.log(`Device '${event.reader.name}' activated, devices: ${devices.listDevices()}`);
+devices.on('reader-attached', function (reader) {
+    console.log(`Reader '${reader.name}' attached`);
 });
 
-devices.on('device-deactivated', function (event) {
-    console.log(`Device '${event.reader.name}' deactivated, devices: ${devices.listDevices()}`);
+devices.on('reader-detached', function (reader) {
+    console.log(`Reader '${reader.name}' detached`);
 });
 
-devices.on('card-removed', function (event) {
-    console.log(`Card removed from '${event.reader.name}' `);
+devices.on('card-removed', function ({ reader }) {
+    console.log(`Card removed from '${reader.name}'`);
 });
 
-devices.on('command-issued', function (event) {
-    console.log(`Command '${event.command}' issued to '${event.reader.name}' `);
+devices.on('error', function (error) {
+    console.log(`Error: ${error.message}`);
 });
 
-devices.on('response-received', function (event) {
-    console.log(`Response '${event.response}' received from '${event.reader.name}' in response to '${event.command}'`);
-});
+devices.on('card-inserted', function ({ reader, card }) {
+    console.log(`Card inserted into '${reader.name}', atr: '${card.atr.toString('hex')}'`);
 
-devices.on('error', function (event) {
-    console.log(`Error '${event.error}' received`);
-});
-
-devices.on('card-inserted', function (event) {
-
-    console.log(`List devices: ${devices.listDevices()}`);
-
-    var reader = event.reader;
-    console.log(`Card inserted into '${reader.name}', atr: '${event.status.atr.toString('hex')}'`);
-
-    var application = iso7816(devices, reader);
+    const application = iso7816(card);
     application
         .selectFile([0x31, 0x50, 0x41, 0x59, 0x2E, 0x53, 0x59, 0x53, 0x2E, 0x44, 0x44, 0x46, 0x30, 0x31])
         .then(function (response) {
@@ -41,5 +30,6 @@ devices.on('card-inserted', function (event) {
         }).catch(function (error) {
             console.error('Error:', error, error.stack);
         });
-
 });
+
+devices.start();
